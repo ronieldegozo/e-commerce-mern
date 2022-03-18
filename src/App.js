3f4c9088b39c71efc745c4966650f7f3ca65a4a8
Mc Roniel De Gozo <ronieldegozo@gmail.com>
2022-03-19 03:51:36 +0800

    Adding a scss file to the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import './categories.styles.scss';
+const  App = () => {
 
-function App() {
+  const categories = [
+    {
+      id: 1,
+      title: 'Monitor' 
+    } ,
+    {
+      id: 2,
+      title: 'Mouse'
+    } ,
+    {
+      id: 3,
+      title: 'Keyboard'
+    } ,
+    {
+      id: 4,
+      title: 'Headphone'
+    } ,
+    {
+      id: 5,
+      title: 'Speaker'
+    } ,
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="cats-container">
+      {categories.map(({title, id})=> (
+        <div key={id} className="cat-container">
+          <div className="background-image"/>
+          <div  className="cat-body-container">
+            <h1>{title}</h1>
+            <h2>Shope now</h2>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
