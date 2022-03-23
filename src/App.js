bd1c196ce1b95f9f90d10911f3044ae9294029d2
Mc Roniel De Gozo <ronieldegozo@gmail.com>
2022-03-23 16:45:31 +0800

    Creating a route for the home page

--- src/App.js
+++ src/App.js
@@ -1,38 +1,12 @@
-import CatContainer from './components/category-container/CatContainer.component';
+import {Routes, Route} from 'react-router-dom'
+import Home from './routes/home/home.component';
+
 const  App = () => {
-
-  const categories = [
-    {
-      id: 1,
-      title: 'Monitor' ,
-      imageUrl: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-    } ,
-    
-    {
-      id: 2,
-      title: 'Mouse',
-      imageUrl: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-    } ,
-    {
-      id: 3,
-      title: 'Keyboard',
-      imageUrl: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-    } ,
-    {
-      id: 4,
-      title: 'Headphone',
-      imageUrl: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-    } ,
-    {
-      id: 5,
-      title: 'Speaker',
-      imageUrl: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-    } ,
-  ]
-
   return (
-    <CatContainer categories={categories} />
-  );
+    <Routes>
+      <Route path='/home' element={<Home/>} />  
+    </Routes>
+  )
 }
 
 export default App;
---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,38 @@
+import CatContainer from '../../components/category-container/CatContainer.component';
+const  Home = () => {
+
+  const categories = [
+    {
+      id: 1,
+      title: 'Monitor' ,
+      imageUrl: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    } ,
+    
+    {
+      id: 2,
+      title: 'Mouse',
+      imageUrl: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    } ,
+    {
+      id: 3,
+      title: 'Keyboard',
+      imageUrl: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    } ,
+    {
+      id: 4,
+      title: 'Headphone',
+      imageUrl: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    } ,
+    {
+      id: 5,
+      title: 'Speaker',
+      imageUrl: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    } ,
+  ]
+
+  return (
+    <CatContainer categories={categories} />
+  );
+}
+
+export default Home;
