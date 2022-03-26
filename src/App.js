b849853f59a4944f9b057fa0722d084b604d1427
Mc Roniel De Gozo <ronieldegozo@gmail.com>
2022-03-27 02:49:52 +0800

    adding a routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
-import {Routes, Route} from 'react-router-dom'
+import {Routes, Route, Outlet} from 'react-router-dom'
 import Home from './routes/home/home.component';
 
-const Shop = () =>{
-  return (
-    <h2>This is a shop route</h2>
+const Navigation = () =>{
+  return(
+    <nav>
+      <ul>
+        <li>
+          <a href="/">Path</a>
+        </li>
+        <li>
+          <a href="/home">Home</a>
+        </li>
+        <li>
+          <a href="/shop">Shop</a>
+        </li>
+      </ul>
+      <Outlet/>
+    </nav>
+  
   )
+}
+
+const Shop = () =>{
+  return <h1>test</h1>
 }
 
 const  App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home/>} >
-        <Route path='shop' element={<Shop/>} />
-      </Route>  
+      <Route path='/' element={<Navigation/>}>
+        <Route index element={<Home/>}/>
+        <Route path='shop' element={<Shop/>}/>
+      </Route>
     </Routes>
   )
 }
